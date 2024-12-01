c5157cc51b49fd4e0fe2504b8d4509a9ad70fbe8
nathan0000 <yaoliang_w@hotmail.com>
2024-12-01 20:49:41 +1100

    spx debug

--- ibwebapi_option_chain.py
+++ ibwebapi_option_chain.py
@@ -1,5 +1,7 @@
 import requests
 import urllib3
+import yaml
+import pandas as pd
 import json, logging, logging.config
 from logging.handlers import RotatingFileHandler
 import csv, json
@@ -32,6 +34,19 @@ def accountInfo(account='DU3232524'):
 
   return json.dumps(account_request.text)
 
+def positionInfo(account=paper_account):
+  url = f'{baseUrl}/portfolio/{account}/positions/0'
+
+  pos_req = requests.get(url=url, verify=False)
+  logger.debug(f'position request status: {pos_req.status_code}')
+  logger.debug(f'position info: {pos_req}')
+
+  if pos_req.status_code == '200':
+    pos_json = json.dumps(pos_req.json(), indent=2)
+    return pos_json
+  else:
+    return pos_req.status_code
+
 def orderInfo():
   url = f'{baseUrl}/iserver/account/orders'
 
@@ -50,17 +65,17 @@ def tradesInfo():
 
   return json.dumps(search_trades.text)
 
+def optionIndex(ticker='SPX'):
+  pass
+
 def optionSamurai(input="samurai.csv"):
-  with open(input, 'r') as samurai_csv:
-    samurai_reader = csv.DictReader(samurai_csv)
-    for row in samurai_reader:
-      symbol = row["Symbol"]
-      legs = row["strategy"].spilit("/")
-      if len(legs) == 0:
-        logger.error("Invalid strategy")
-        return
-      for i in range(len(legs)):
-        contract[i] = legs[i].split(" ")
+  ds_samurai = pd.read_csv(input, delimiter=',')
+  ds_samurai_work = ds_samurai[["Include symbols",  "Options"]]
+#  ds_samurai_work["Strike"] = ds_samurai_work["Strike"].apply(lambda x: x.split('/'))
+#  ds_samurai_work["Options"] = ds_samurai_work["Options"].apply(lambda x: x.split('/'))
+#  ds_samurai_work.explode("Options")
+  print(ds_samurai_work.dtypes)
+'''
       underConid,months = secdefSearch(symbol, exchange)
       month = months[0]
       itmStrikes = secdefStrikes(underConid,month)
@@ -70,6 +85,7 @@ def optionSamurai(input="samurai.csv"):
       writeResult(contractDict)
 
   return
+'''
 
 def orderCombo(account="DU3232524"):
   us_spread_conid = "28812380"
@@ -139,6 +155,7 @@ def secdefSearch(symbol, listingExchange):
 
   search_request = requests.get(url=url, verify=False)
   logger.debug('search request code: {}'.format(search_request.status_code))
+  logger.debug(f'sec def search response: {search_request.json()}')
 
   for contract in search_request.json():
     if contract["description"] == listingExchange:
@@ -153,12 +170,15 @@ def secdefSearch(symbol, listingExchange):
 def secdefStrikes(underConid,month):
 
   snapshot = float(snapshotData(underConid))
+#  snapshot = snapshotData(underConid)
+
   itmStrikes = []
 
   url = f'{baseUrl}/iserver/secdef/strikes?conid={underConid}&secType=OPT&month={month}'
 
   strike_request = requests.get(url=url, verify=False)
   logger.debug('strike_request status code: {}'.format(strike_request.status_code))
+  logger.debug(f'strike request response: {strike_request.json()}')
 
   strikes = strike_request.json()["put"]
   for strike in strikes:
@@ -176,12 +196,18 @@ def secdefInfo(conid, month, strike, right='P'):
   contracts = []
 
   for contract in info_request.json():
+    # add option ask/bid price&size
+    optionPrice = snapshotOption(contract["conid"])
+    optionPrice_string = json.dumps(optionPrice)
+
     contractDetails = {"conid": contract["conid"], 
                        "symbol": contract["symbol"],
                        "strike": contract["strike"],
+                       "prices": optionPrice_string,
                        "maturityDate": contract["maturityDate"]
                       }
     contracts.append(contractDetails)
+  logger.debug(f"contract details: {contracts}")
   return contracts
 
 def snapshotData(underConid):
@@ -189,10 +215,23 @@ def snapshotData(underConid):
   requests.get(url=url, verify=False)
   snapshot = requests.get(url=url, verify=False)
   logger.debug('snapshot_request status code: {}'.format(snapshot.status_code))
+  logger.debug('snapshotData price: {}'.format(snapshot.json()[0]))
   return snapshot.json()[0]["31"]
 
+def snapshotOption(optionConid):
+  url = f'{baseUrl}/iserver/marketdata/snapshot?conids={optionConid}&fields=84,88,86,85'
+  optionPrices = {}
+  requests.get(url=url, verify=False)
+  optionSnapshot = requests.get(url=url, verify=False)
+  logger.debug('options snapshot_request status code: {}'.format(optionSnapshot.status_code))
+  logger.debug('option snapshot data: {}'.format(optionSnapshot.json()[0]))
+  optionPrices = {"bid": optionSnapshot.json()[0]["84"],"bidSize": optionSnapshot.json()[0]["88"], \
+                      "ask": optionSnapshot.json()[0]["86"],"askSize": optionSnapshot.json()[0]["85"]}
+  logger.debug(f"contract: {optionConid} option prices: {optionPrices}")
+  return optionPrices
+
 def writeResult(contractDict):
-  headers = ["conid", "symbol", "strike", "maturityDate"]
+  headers = ["conid", "symbol", "strike", "optionPrices", "maturityDate"]
   filePath = "./MayContracts.csv"
   contract_csv_file = open(filePath, 'w', newline='')
   contract_writer = csv.DictWriter(f=contract_csv_file, fieldnames=headers)
@@ -202,6 +241,13 @@ def writeResult(contractDict):
       contract_writer.writerow(contractDetails)
   contract_csv_file.close()
   print("Job's done.")
+
+def writeJsonResult(contractDict):
+  filePath = "./MayContracts.json"
+  contractJson = json.dumps(contractDict, indent=2)
+  with open(filePath, 'w', newline='') as contract_json_file:
+    contract_json_file.write(contractJson)
+  print("Json file written.")
 
 if __name__ == "__main__":
   
@@ -215,42 +261,51 @@ if __name__ == "__main__":
   # Setup Logging
   logger = loggerSetup()
 
+  # get position info
+#  positions = positionInfo(paper_account)
+#  print(f'postions: {positions}')
+
+  # parse option samurai
+  #optionSamurai("./samurai.csv")
+
   # return account info
-  account_info = accountInfo()
-  logger.debug('account info: {}'.format(account_info))
+  #account_info = accountInfo()
+  #logger.debug('account info: {}'.format(account_info))
 
   # search order info
-  order_info = orderInfo()
-  print(f'order info: {order_info}')
+  #order_info = orderInfo()
+  #print(f'order info: {order_info}')
 
   # search trades info
-  trades_info = tradesInfo()
-  print(f'trades info: {trades_info}')
+  #trades_info = tradesInfo()
+  #print(f'trades info: {trades_info}')
 
   #prepare and submit order
-  order_data = orderSimple()
-  logger.debug(f'order data: {json.dumps(order_data)}')
-  order_response = orderPlace(paper_account, order_data)
-  print(f'order response : {order_response}')
-  print(f'messageIds: {order_response[0]["messageIds"]}')
+  #order_data = orderSimple()
+  #logger.debug(f'order data: {json.dumps(order_data)}')
+  #order_response = orderPlace(paper_account, order_data)
+  #print(f'order response : {order_response}')
+  #print(f'messageIds: {order_response[0]["messageIds"]}')
   #handle order reply message
-  if order_response[0]["messageIds"][0] == "o163":
-    replyId = order_response[0]["id"]
-    order_reply = orderReply(replyId, True)
-    logger.debug(f'order confirmed: {order_reply}')
+  #if order_response[0]["messageIds"][0] == "o163":
+  #  replyId = order_response[0]["id"]
+  #  order_reply = orderReply(replyId, True)
+  #  logger.debug(f'order confirmed: {order_reply}')
+
   # I'm looking for the U.S. Apple Incorporated company listed on NASDAQ
-  #underConid,months = secdefSearch("AAPL", "NASDAQ")
-  
+  underConid,months = secdefSearch("AAPL", "NASDAQ")
+  logger.debug(f'security def search: {underConid, months}')
   # I only want the front month. 
   # Users could always grab all months, or pull out a specific value, but sending the 0 value always gives me the first available contract.
-  #month = months[0]
+  month = months[0]
 
   # We'll be calling our Strikes endpoint to pull in the money strike prices rather than all strikes.
-  #itmStrikes = secdefStrikes(underConid,month)
+  itmStrikes = secdefStrikes(underConid,month)
+  print(f'item strike: {itmStrikes}')
 
   # We can then pass those strikes to the /info endpoint, and retrieve all the contract details we need.
-  #contractDict = {}
-  #for strike in itmStrikes:
-  #  contractDict[strike] = secdefInfo(underConid,month,strike)
+  contractDict = {}
+  for strike in itmStrikes:
+    contractDict[strike] = secdefInfo(underConid,month,strike)
 
-  #writeResult(contractDict)
+  writeJsonResult(contractDict)
